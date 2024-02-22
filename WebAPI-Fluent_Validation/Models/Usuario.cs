6a41d39a4b8ec28c5fe4caeca7d8583d78019e9d
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2024-02-22 14:51:46 -0300

    ajuste para não trazer o id no body da requisição, e autocrementar de forma randomica o Id no construtor.

--- WebAPI-Fluent_Validation/Models/Usuario.cs
+++ WebAPI-Fluent_Validation/Models/Usuario.cs
@@ -1,10 +1,21 @@
 ﻿using FluentValidation;
 using FluentValidation.Results;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace WebAPI_Fluent_Validation.Models;
 
 public class Usuario
 {
+    public Usuario(string nome, string email, int idade)
+    {
+        Id = new Random()
+            .Next(0, 101);
+        Nome = nome;
+        Email = email;
+        Idade = idade;
+    }
+    [JsonIgnore]
     public int Id { get; set; }
     public string Nome { get; set; }
     public string Email { get; set; }
